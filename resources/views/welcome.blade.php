7ea4c3ef4554f3bacb0c4846187cd3e01abf2f00
gunjan gupta <gunjan.gupta@dotsquares.com>
2023-04-17 09:51:29 +0530

    linked jenkins to git repo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -459,7 +459,7 @@
                         </div>
 
                         <div class="ml-12">
-                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">The easiest way 
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">The Firt Git Commit
                             </div>
                         </div>
                     </div>
