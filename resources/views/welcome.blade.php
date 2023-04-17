fac55999589e319233053608734ef4f67ac7d955
gunjan gupta <gunjan.gupta@dotsquares.com>
2023-04-17 09:55:15 +0530

    add code to live server through jenkins

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -459,7 +459,7 @@
                         </div>
 
                         <div class="ml-12">
-                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">The Firt Git Commit
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">The Second Git Commit to make build on LIVE server
                             </div>
                         </div>
                     </div>
