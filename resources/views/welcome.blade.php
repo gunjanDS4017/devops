9701dbc26c99f08bcec7e27a6bf1de6453733702
gunjan gupta <gunjan.gupta@dotsquares.com>
2023-04-19 16:39:15 +0530

    pipeline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -459,8 +459,7 @@
                         </div>
 
                         <div class="ml-12">
-                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">Set Pipeline
-                            </div>
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">Set Pipeline2</div>
                         </div>
                     </div>
                 </div>
