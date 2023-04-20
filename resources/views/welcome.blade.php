45d97c05e15c21915cfda339cbf567f333f21ee6
gunjan gupta <gunjan.gupta@dotsquares.com>
2023-04-20 17:59:31 +0530

    test email body

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -459,7 +459,7 @@
                         </div>
 
                         <div class="ml-12">
-                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">Trigger Pipeline with email notification with CC</div>
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">16th commit - Trigger Pipeline with email body</div>
                         </div>
                     </div>
                 </div>
